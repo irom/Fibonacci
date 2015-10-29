9ba8753929db48870a5e8301e1fade9cfb41992b
Ivan <romanov75@gmail.com>
2015-10-29 13:45:32 -0400

    commit

---
+++ Fibonacci/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Fibonacci
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public string Hobbies { get; set; }
+    }
+}
